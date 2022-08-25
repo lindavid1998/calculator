b5a8f70e7d79e681e8d8484fcb125656f16bafbd
David Lin <lindavid1998@gmail.com>
2022-08-24 22:20:37 -0700

    Add operate function

--- javascript.js
+++ javascript.js
@@ -14,6 +14,19 @@ function divide(a, b) {
     return a / b;
 }
 
+function operate(a, b, operator) {
+    switch (true) {
+        case (operator === '+'):
+            return add(a, b)
+        case (operator === '-'):
+            return subtract(a, b)
+        case (operator === '*'):
+            return multiply(a, b)
+        case (operator === '/'):
+            return divide(a, b)
+    }
+}
+
 // Tests
 function assertEqual(expected, actual) {
     if (expected === actual) {
@@ -23,7 +36,11 @@ function assertEqual(expected, actual) {
     }
 }
 
-assertEqual(5, add(2,3))
+assertEqual(5, add(2, 3))
 assertEqual(6, subtract(9, 3))
 assertEqual(8, multiply(2, 4))
 assertEqual(2, divide(10, 5))
+assertEqual(5, operate(2, 3, '+'))
+assertEqual(6, operate(9, 3, '-'))
+assertEqual(8, operate(2, 4, '*'))
+assertEqual(2, operate(10, 5, '/'))
