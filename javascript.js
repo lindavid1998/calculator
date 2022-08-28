3bc89bc0b3229970c2af1d5ac31136a5bebe1795
David Lin <lindavid1998@gmail.com>
2022-08-28 13:57:00 -0700

    Move CSS and javascript to style.css and javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,83 @@
+let firstNum;
+let secondNum;
+let operator;
+let overwrite = false;
+let operatorActive = false;
+
+// Update display when numbers are pressed
+function displayNums(e) {
+    // disable . button if already pressed
+    if (display.textContent.includes('.') && e.target.textContent === '.') {
+        return
+    }
+    if (overwrite) {
+        display.textContent = ''
+        document.getElementById(`${operator}`).classList.remove('active')
+        overwrite = false
+    }
+    display.textContent += e.target.textContent;
+};
+let display = document.querySelector('.display');
+let numbers = Array.from(document.querySelectorAll('.pad button'));
+numbers.filter(number => number.textContent !== '=').forEach(number => (
+    number.addEventListener('click', displayNums)
+));
+
+// Clear display when clear button is pressed
+document.querySelector('.clear').addEventListener('click', () => {
+    display.textContent = '';
+    overwrite = false;
+    operatorActive = false;
+    firstNum = null;
+    secondNum = null;
+    operator = null;
+    buttons.forEach(button => button.classList.remove('active'))
+})
+
+function applyOperator(e) {
+    // Run calculation if operator is already active
+    if (operatorActive) {
+        secondNum = display.textContent;
+        display.textContent = `${operate(firstNum, secondNum, operator)}`
+        document.getElementById(`${operator}`).classList.remove('active') // remove highlight on previous operator pressed
+    }
+    firstNum = display.textContent;
+    operator = e.target.id;
+    overwrite = true;
+    operatorActive = true;
+    e.target.classList.add('active')
+}
+let buttons = document.querySelectorAll('.operator')
+buttons.forEach(button => button.addEventListener('click', applyOperator))
+
+// Run calculation when = is pressed 
+let equal = document.querySelector('#eq')
+equal.addEventListener('click', () => {
+    if (firstNum === null) {
+        return
+    }
+    secondNum = display.textContent
+    display.textContent = `${operate(firstNum, secondNum, operator)}`
+    document.getElementById(`${operator}`).classList.remove('active')
+    overwrite = true
+    operatorActive = false
+    firstNum = null;
+}) 
+
+// animate buttons
+function animateButton(e) {
+    if (e.type === 'mousedown') {
+        e.target.style.backgroundColor = '#388'
+    } else {
+        e.target.style.backgroundColor = ''
+    }
+}
+numbers.forEach(number => number.addEventListener('mousedown', animateButton))
+numbers.forEach(number => number.addEventListener('mouseup', animateButton))
+document.querySelector('.clear').addEventListener('mousedown', animateButton)
+document.querySelector('.clear').addEventListener('mouseup', animateButton)
+
+// operations
 function add(a, b) {
     return Number(a) + Number(b);
 }
@@ -11,6 +91,7 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
+    if (b === '0') return 'divide by 0 error'
     return a / b;
 }
 
@@ -26,21 +107,3 @@ function operate(a, b, operator) {
             return divide(a, b)
     }
 }
-
-// Tests
-function assertEqual(expected, actual) {
-    if (expected === actual) {
-        console.log('passed')
-    } else {
-        console.log(`failed. expected ${expected} but got ${actual}`)
-    }
-}
-
-assertEqual(5, add(2, 3))
-assertEqual(6, subtract(9, 3))
-assertEqual(8, multiply(2, 4))
-assertEqual(2, divide(10, 5))
-assertEqual(5, operate(2, 3, '+'))
-assertEqual(6, operate(9, 3, '-'))
-assertEqual(8, operate(2, 4, '*'))
-assertEqual(2, operate(10, 5, '/'))
